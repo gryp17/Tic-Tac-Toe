6139f55bb490e428bd1a19ec68065ecbd24a57e4
usuario <usuario@usuario-VirtualBox>
2017-04-25 21:56:07 +0200

    Changed the login/signup initial form tabs

--- public/scripts/pages/index.js
+++ public/scripts/pages/index.js
@@ -1,65 +1,75 @@
 function index() {
 
-    function login() {
+	//tabs view behaviour
+	$(".tab-button").click(function () {
+		$(".tab-button").removeClass("active");
+		$(this).addClass("active");
 
-        $.ajax({
-            url: "/login",
-            type: "POST",
-            data: $("#login-form").serialize()
-        }).done(function (result) {
-            if (result.user) {
-                window.location.href = "/lobby";
-            } else {
-                toastr.error(result);
-            }
-        });
+		var target = $(this).attr("data-target");
+		$(".tab-content").removeClass("active");
+		$("#" + target).addClass("active");
+	});
 
-    }
+	/**
+	 * Login ajax request
+	 */
+	function login() {
+		$.ajax({
+			url: "/login",
+			type: "POST",
+			data: $("#login-form").serialize()
+		}).done(function (result) {
+			if (result.user) {
+				window.location.href = "/lobby";
+			} else {
+				toastr.error(result);
+			}
+		});
+	}
 
-    //log in
-    $("#login-form button").click(function () {
-        login();
-    });
+	//log in
+	$("#login-form button").click(function () {
+		login();
+	});
 
-    //text input handler
-    $("#login-form input[type=password]").keypress(function (e) {
-        console.log("bla bla");
-        if (e.which === 13) {
-            login();
-        }
-    });
+	//text input handler
+	$("#login-form input[type=password]").keypress(function (e) {
+		if (e.which === 13) {
+			login();
+		}
+	});
 
 
-    //sign up and log in the user
-    $("#signup-form .signup-btn").click(function () {
+	//sign up and log in the user
+	$("#signup-form .signup-btn").click(function () {
 
-        var formData = new FormData(document.getElementById("signup-form"));
+		var formData = new FormData(document.getElementById("signup-form"));
 
-        $.ajax({
-            url: "/signup",
-            type: "POST",
-            enctype: "multipart/form-data",
-            processData: false,
-            contentType: false,
-            data: formData
-        }).done(function (result) {
-            if (result.user) {
-                window.location.href = "/lobby";
-            } else {
-                toastr.error(result);
-            }
-        });
+		$.ajax({
+			url: "/signup",
+			type: "POST",
+			enctype: "multipart/form-data",
+			processData: false,
+			contentType: false,
+			data: formData
+		}).done(function (result) {
+			if (result.user) {
+				window.location.href = "/lobby";
+			} else {
+				toastr.error(result);
+			}
+		});
 
-    });
+	});
 
-    //redirect the click to the actual file input
-    $(".browse-btn, .avatar-preview").click(function () {
-        $(".avatar").click();
-    });
+	//redirect the click to the actual file input
+	$(".browse-btn, .avatar-preview").click(function () {
+		$(".avatar").click();
+	});
 
-    //on avatar file change generate the preview
-    $(".avatar").change(function (e) {
-        $(".avatar-preview").attr("src", URL.createObjectURL(e.target.files[0]));
-    });
+	//on avatar file change generate the preview
+	$(".avatar").change(function (e) {
+		$(".avatar-preview").attr("src", URL.createObjectURL(e.target.files[0]));
+	});
 
 }
