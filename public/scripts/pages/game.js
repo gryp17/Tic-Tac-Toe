d38a640323c591a7b91ba625e7b97e3797c07697
plamen <plamen.ivanov17@gmail.com>
2018-02-01 12:16:54 +0200

    Added the basic gameMap, implemented the "makeMove" functionality and renamed a bunch of variables and functions

--- public/scripts/pages/game.js
+++ public/scripts/pages/game.js
@@ -1,10 +1,53 @@
 function game(){
+	var myId = parseInt($("#user-id").val());
+	
 	var socket = io("/game");
 	
-	//start game handler
-	socket.on("startGame", function (playerId) {
-		console.log("the game has started!");
-		console.log("it's "+playerId+" turn");
+	//update game handler
+	socket.on("updateGame", function (game) {
+		console.log("it's player "+game.playerTurn+" turn");
+		
+		console.log(game);
+		
+		$("#game-map").empty();
+		
+		//generate the game map
+		game.gameMap.forEach(function (row, rowIndex){
+			var tr = $("<tr>");
+			
+			row.forEach(function (value, colIndex){
+				tr.append($("<td>", {
+					class: rowIndex+"-"+colIndex,
+					text: mapCellValue(value),
+					click: makeMove
+				}));
+			});
+			
+			$("#game-map").append(tr);
+		});
 	});
+	
+	/**
+	 * Maps the gameMap value to the correct symbol or image
+	 * @param {Number} value
+	 * @returns {String}
+	 */
+	function mapCellValue(value){
+		if(value === myId){
+			return "X";
+		}else if(value === 0){
+			return "";
+		}else{
+			return "O";
+		}
+	}
+	
+	/**
+	 * Sends the makeMove event passing the coordinates of the selected cell
+	 */
+	function makeMove(){
+		var cell = $(this).attr("class");
+		socket.emit("makeMove", cell);
+	}
 	
 }
--- server/sockets/namespaces/game.js
+++ server/sockets/namespaces/game.js
@@ -14,24 +14,90 @@ module.exports = function (io, app) {
 		var lobby = app.get("socketNamespaces").lobby;
 		
 		//find the game that this user belongs to
-		var activeGame = lobby.findGameByUserId(socket.session.user.id, "active");		
-		var gameRoomId = activeGame.players[0].id+"-"+activeGame.players[1].id;
+		var myGame = game.getMyGame(lobby, socket);	
+		var gameRoomId = myGame.players[0].id+"-"+myGame.players[1].id;
 		
 		//join a room that is reserved only for the players in this game
 		socket.join(gameRoomId);
 		
 		//emit the start game event to both players and indicate that it's the first player's turn
-		activeGame.turn = activeGame.players[0].id;
-		game.to(gameRoomId).emit("startGame", activeGame.turn);
+		myGame.playerTurn = myGame.players[0].id;
+		game.updateGame(myGame);
+		
+		//make move handler
+		socket.on("makeMove", function (coordinates){
+			var myGame = game.getMyGame(lobby, socket);
+			
+			coordinates = coordinates.split("-");
+			var x = coordinates[0];
+			var y = coordinates[1];
+			
+			//check if the selected cell is empty and if it's the current player's turn
+			if(myGame.gameMap[x][y] === 0 && myGame.playerTurn === socket.session.user.id){
+				//make the move
+				myGame.gameMap[x][y] = socket.session.user.id;
+				
+				//switc the player turn
+				myGame = game.switchPlayerTurn(myGame);
+				
+				//update the game
+				game.updateGame(myGame);
+			}
+		});
 
 		//disconnect event handler
 		socket.on("disconnect", function () {
-
+			//TODO: send chat message
 			console.log("@@@@@ user disconnected");
-			
 		});
 
 	});
+	
+	/**
+	 * Returns the active game that this socket/user belongs to
+	 * @param {Object} lobbyNamespace
+	 * @param {Object} socket
+	 * @returns {Object}
+	 */
+	game.getMyGame = function (lobbyNamespace, socket){
+		return lobbyNamespace.findGameByUserId(socket.session.user.id, "active");
+	};
+		
+	/**
+	 * Broadcasts the game object to all players in that game
+	 * @param {Object} myGame
+	 */
+	game.updateGame = function (myGame){
+		var gameRoomId = myGame.players[0].id+"-"+myGame.players[1].id;
+		
+		//initialize an empty game map if the map is not set yet
+		if(!myGame.gameMap){
+			myGame.gameMap = [
+				[0, 0, 0],
+				[0, 0, 0],
+				[0, 0, 0]
+			];
+		}
+				
+		game.to(gameRoomId).emit("updateGame", myGame);
+	};
+	
+	/**
+	 * Switches the current player turn
+	 * @param {Object} myGame
+	 * @returns {Object}
+	 */
+	game.switchPlayerTurn = function (myGame){
+		var currentPlayer = myGame.playerTurn;
+		
+		myGame.players.forEach(function (player){
+			if(currentPlayer !== player.id){
+				myGame.playerTurn = player.id;
+			}
+		});
+		
+		return myGame;
+	};
 
 	return game;
 };
